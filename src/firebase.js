d40ac0514633cb93ffeb840138ddf8060b3e8cce
PhamDuyHau <127919185+PhamDuyHau@users.noreply.github.com>
2025-04-09 16:01:03 +0700

    Up

--- src/firebase.js
+++ src/firebase.js
@@ -2,27 +2,26 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// Your Firebase configuration
-const firebaseConfig = {
-    authDomain: 'hau-facebook.firebaseapp.com',
-    projectId: 'hau-facebook',
-    storageBucket: 'hau-facebook.firebasestorage.app',
-    messagingSenderId: '346298572774',
-    appId: '1:346298572774:web:3ca4865a454041d2720027',
-    measurementId: 'G-623B3LFVBQ',
-};
 
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 
-
 const db = getFirestore(app);
 
+const auth = getAuth(app);
 
-const auth = getAuth(app); // This initializes Firebase Authentication
-
-
-const provider = new GoogleAuthProvider(); // Google Auth provider to sign in
-
+const provider = new GoogleAuthProvider();
 
 export { auth, provider, signInWithPopup, db };
+
+console.log('Firebase API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+
